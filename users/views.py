dd4f75f98afdaebd6ff82deeb993fb2b54be8414
mwfarb <mwfarb@andrew.cmu.edu>
2020-11-05 11:11:47 -0500

    basic login, crispy forms

--- users/views.py
+++ users/views.py
@@ -1,23 +1,51 @@
-from django.shortcuts import  render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import redirect, render
+
 from .forms import NewUserForm
-from django.contrib.auth import login
-from django.contrib import messages #import messages
 from .models import Scene
 
 
 def homepage(request):
-	scenes = Scene.objects.all() #queryset containing all scenes we just created
-	return render(request=request, template_name="users/home.html", context={'scenes':scenes})
+    scenes = Scene.objects.all()  # queryset containing all scenes we just created
+    return render(request=request, template_name="users/home.html", context={'scenes': scenes})
 
 
 def register_request(request):
-	if request.method == "POST":
-		form = NewUserForm(request.POST)
-		if form.is_valid():
-			user = form.save()
-			login(request, user)
-			messages.success(request, "Registration successful." )
-			return redirect("users:homepage")
-		messages.error(request, "Unsuccessful registration. Invalid information.")
-	form = NewUserForm
-	return render (request=request, template_name="users/register.html", context={"register_form":form})
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful.")
+            return redirect("users:homepage")
+        messages.error(
+            request, "Unsuccessful registration. Invalid information.")
+    form = NewUserForm
+    return render(request=request, template_name="users/register.html", context={"register_form": form})
+
+
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect("users:homepage")
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request=request, template_name="users/login.html", context={"login_form": form})
+
+
+def logout_request(request):
+    logout(request)
+    messages.info(request, "You have successfully logged out.")
+    return redirect("users:homepage")
