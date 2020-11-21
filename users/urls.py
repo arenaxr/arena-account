bba9b5d436530f6ef6c571a3ada4084f07ce21dc
mwfarb <mwfarb@andrew.cmu.edu>
2020-11-21 16:58:00 -0500

    add use state feedback

--- users/urls.py
+++ users/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path("profile/<username>", views.user_profile, name="user_profile"),
     path("login_callback", views.login_callback, name="login_callback"),
     path('mqtt_auth', views.mqtt_token, name = "mqtt_token"),
+    path('user_state', views.user_state, name = "user_state"),
 ]
