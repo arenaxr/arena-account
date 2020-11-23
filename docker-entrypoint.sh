608f1e54605dfb4a68b0458fac951ed41277ee03
nampereira <3504501+nampereira@users.noreply.github.com>
2020-11-23 16:19:35 -0500

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+python manage.py makemigrations --noinput
+python manage.py migrate   
+python manage.py collectstatic --noinput
+
 export DJANGO_SETTINGS_MODULE=arena_account.settings
-python manage.py createsuperuser --noinput || python -c "\
+bash -c 'python manage.py createsuperuser --noinput' || true
+# if above command fails because user exists, change user password
+python -c "\
 import django; django.setup(); \
 import os; \
 USER = os.getenv('DJANGO_SUPERUSER_USERNAME'); \
@@ -9,7 +15,6 @@ PASS = os.getenv('DJANGO_SUPERUSER_PASSWORD'); \
 from django.contrib.auth.models import User; \
 u = User.objects.get(username=USER); \
 u.set_password(PASS);\
-u.save();"
+u.save();" || true
   
-python manage.py collectstatic --noinput
 python manage.py runserver 0.0.0.0:8000
