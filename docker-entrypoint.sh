01f753c547a031ff5a04a75b6c8e7cc666bd93f8
nampereira <3504501+nampereira@users.noreply.github.com>
2020-11-23 12:10:58 -0500

    Remove create admin user

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-#create admin user
-export DJANGO_SETTINGS_MODULE=arena_account.settings
-python -c "import django; django.setup(); \
-   from django.contrib.auth.management.commands.createsuperuser import get_user_model; \
-   get_user_model()._default_manager.db_manager('$DJANGO_DB_NAME').create_superuser( \
-   username='$ACCOUNT_ADMIN_NAME', \
-   email='$ACCOUNT_ADMIN_EMAIL', \
-   password='$ACCOUNT_ADMIN_PASSWORD')"
-   
 python manage.py makemigrations --noinput
 python manage.py migrate 
 python manage.py collectstatic --noinput
