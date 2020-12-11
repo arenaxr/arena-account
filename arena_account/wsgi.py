bea89f42755c5ae10057a1135d8cfbf6806263cd
mwfarb <mwfarb@andrew.cmu.edu>
2020-12-11 14:30:22 -0500

    adding startup hook for scene population

--- arena_account/wsgi.py
+++ arena_account/wsgi.py
@@ -7,10 +7,27 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
+import datetime
 import os
 
 from django.core.wsgi import get_wsgi_application
+from users.models import Scene
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'arena_account.settings')
+
+##############
+# TODO: add unknown scenes to scene database
+# TODO: get key for persist
+# TODO: request all scenes from persist
+# TODO: add only-missing scenes to scene database
+
+now = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+s = Scene(
+    name=f'example-{now}',
+    summary='An example scene.',
+)
+print(f'Adding scene example-{now}')
+s.save()
+#################
 
 application = get_wsgi_application()
--- users/models.py
+++ users/models.py
@@ -1,16 +1,31 @@
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.urls import reverse
 
 
 class Scene(models.Model):
     """Model representing a scene (but not a specific copy of a scene)."""
-    title = models.CharField(max_length=200)
-    summary = models.TextField(
-        max_length=1000, help_text='Enter a brief description of the scene')
+    name = models.CharField(max_length=200, blank=False, unique=True)
+    summary = models.TextField(max_length=1000)
+    editors = models.ManyToManyField(User)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    public_read = models.BooleanField(default=True)
+    public_write = models.BooleanField(default=True)
+
+    def save(self, *args, **kwargs):
+        self.full_clean()  # performs regular validation then clean()
+        super(Scene, self).save(*args, **kwargs)
+
+    def clean(self):
+        if self.name == '':
+            raise ValidationError('Empty scene name!')
+        self.name = self.name.strip()
 
     def __str__(self):
-        """String for representing the Model object."""
-        return self.title
+        """String for representing the scene object by name."""
+        return self.name
 
     def get_absolute_url(self):
         """Returns the url to access a detail record for this scene."""
-        return reverse('scene-detail', args=[str(self.id)])
+        return reverse('scene-detail', args=[str(self.name)])
