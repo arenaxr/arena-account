53508a007ce66ff0f2964926f13483b3393a81b6
mwfarb <mwfarb@andrew.cmu.edu>
2021-01-13 17:49:17 -0500

    disable standard email accounts

--- arena_account/urls.py
+++ arena_account/urls.py
@@ -22,13 +22,13 @@ from users import views
 urlpatterns = [
     path('user/', include('users.urls')),
     path('user/admin/', admin.site.urls),
-    #path('user/accounts/', include('django.contrib.auth.urls')),
-    path('user/password_reset/done/', auth_views.PasswordResetDoneView.as_view(
-        template_name='users/password/password_reset_done.html'), name='password_reset_done'),
-    path('user/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
-        template_name="users/password/password_reset_confirm.html"), name='password_reset_confirm'),
-    path('user/reset/done/', auth_views.PasswordResetCompleteView.as_view(
-        template_name='users/password/password_reset_complete.html'), name='password_reset_complete'),
+    # path('user/accounts/', include('django.contrib.auth.urls')),
+    # path('user/password_reset/done/', auth_views.PasswordResetDoneView.as_view(
+    #     template_name='users/password/password_reset_done.html'), name='password_reset_done'),
+    # path('user/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+    #     template_name="users/password/password_reset_confirm.html"), name='password_reset_confirm'),
+    # path('user/reset/done/', auth_views.PasswordResetCompleteView.as_view(
+    #     template_name='users/password/password_reset_complete.html'), name='password_reset_complete'),
     path('user/accounts/social/signup',
          views.socialaccount_signup, name='socialaccount_signup'),
     path('user/accounts/', include('allauth.urls')),
--- users/urls.py
+++ users/urls.py
@@ -4,10 +4,10 @@ from . import startup, views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("register", views.register_request, name="register"),
+    #path("register", views.register_request, name="register"),
     path("login", views.login_request, name="login"),
     path("logout", views.logout_request, name="logout"),
-    path("password_reset", views.password_reset_request, name="password_reset"),
+    #path("password_reset", views.password_reset_request, name="password_reset"),
     path("profile", views.user_profile, name="user_profile"),
     path("login_callback", views.login_callback, name="login_callback"),
     path('mqtt_auth', views.mqtt_token, name="mqtt_token"),
