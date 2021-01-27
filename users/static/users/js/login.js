bb79f9ec712e3267a48229bdddedc0a33beb491e
mwfarb <mwfarb@andrew.cmu.edu>
2021-01-27 13:17:05 -0500

    fix redirects to requested page

--- users/static/users/js/login.js
+++ users/static/users/js/login.js
@@ -20,9 +20,9 @@ var initAnonAuth = function() {
         anonBtn.addEventListener('click', function(event) {
             // check for saved name, use it
             var savedName = localStorage.getItem("display_name");
-            if (savedName !== null) {
+            if (savedName !== null && savedName != 'undefined') {
                 localStorage.setItem("auth_choice", "anonymous");
-                location.href = "../..";
+                returnToRequestedPage();
             } else { // if no passable name, ask for one
                 var namePanel = document.getElementById('anonNameWrapper');
                 namePanel.hidden = false;
@@ -35,10 +35,20 @@ var initAnonAuth = function() {
         var displayName = usernameInput.value.replace(/\s+/g, " ").trim();
         localStorage.setItem("display_name", displayName);  // save for next use
         localStorage.setItem("auth_choice", "anonymous");
-        location.href = "../..";
+        returnToRequestedPage();
     }
     var usernameInput = document.getElementById('usernameInput');
     var nameBtn = document.getElementById('btnAnonName');
     nameBtn.addEventListener('click', formHandler);
     //TODO: might interfere with document.getElementById('login-form').addEventListener('submit', formHandler);
+}
+
+function returnToRequestedPage() {
+    if (localStorage.getItem("request_uri")) {
+        //redirect user to originally requested page
+        location.href = localStorage.getItem("request_uri");
+    } else {
+        //redirect user to root page
+        location.href = `${window.location.protocol}//${window.location.host}`;
+    }
 }
