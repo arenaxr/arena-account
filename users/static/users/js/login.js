5387086fcacf007837f43855a822392cb2d84513
Ivan Liang <r33t@cmu.edu>
2021-02-04 15:50:25 -0500

    Replace current login sequence with new UI

--- users/static/users/js/login.js
+++ users/static/users/js/login.js
@@ -1,48 +1,3 @@
-window.onload = function() {
-    // TODO: temp remove initEmailAuth();
-    initAnonAuth();
-};
-
-var initEmailAuth = function() {
-    var emailBtn = document.getElementById('customBtnEmail');
-    if (typeof emailBtn != 'undefined') {
-        emailBtn.addEventListener('click', function(event) {
-            var emailLoginPanel = document.getElementById('emailLoginWrapper');
-            emailLoginPanel.hidden = false;
-            localStorage.setItem("auth_choice", "arena");
-        });
-    }
-}
-
-var initAnonAuth = function() {
-    var anonBtn = document.getElementById('customBtnAnon');
-    if (typeof anonBtn != 'undefined') {
-        anonBtn.addEventListener('click', function(event) {
-            // check for saved name, use it
-            var savedName = localStorage.getItem("display_name");
-            if (savedName !== null && savedName != 'undefined') {
-                localStorage.setItem("auth_choice", "anonymous");
-                returnToRequestedPage();
-            } else { // if no passable name, ask for one
-                var namePanel = document.getElementById('anonNameWrapper');
-                namePanel.hidden = false;
-            }
-        });
-    }
-    const formHandler = (e) => {
-        e.preventDefault();
-        // remove extra spaces
-        var displayName = usernameInput.value.replace(/\s+/g, " ").trim();
-        localStorage.setItem("display_name", displayName);  // save for next use
-        localStorage.setItem("auth_choice", "anonymous");
-        returnToRequestedPage();
-    }
-    var usernameInput = document.getElementById('usernameInput');
-    var nameBtn = document.getElementById('btnAnonName');
-    nameBtn.addEventListener('click', formHandler);
-    //TODO: might interfere with document.getElementById('login-form').addEventListener('submit', formHandler);
-}
-
 function returnToRequestedPage() {
     if (localStorage.getItem("request_uri")) {
         //redirect user to originally requested page
@@ -52,3 +7,86 @@ function returnToRequestedPage() {
         location.href = `${window.location.protocol}//${window.location.host}`;
     }
 }
+
+// ** New UI **
+
+function showEl(el, flex = false) {
+    const showClass = flex ? 'd-flex' : 'd-block';
+    el.classList.add(showClass);
+    el.classList.remove('d-none');
+}
+
+function hideEls(els, flex = false) {
+    const showClass = flex ? 'd-flex' : 'd-block';
+    for (let el of els) {
+        el.classList.add('d-none');
+        el.classList.remove(showClass);
+    }
+}
+
+function changePage(page = 'landing') {
+    if (window.location.hash !== page) {
+        window.location.hash = page;
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {   // document.ready() equiv
+    window.addEventListener('hashchange', function () {
+        const validRoutes = [
+            '#landing',
+            '#signIn',
+        ]
+        const routePage = validRoutes.includes(window.location.hash) ? window.location.hash : '#landing';
+        const pageEl = document.querySelector(routePage);
+        hideEls(document.querySelectorAll(`.routePage:not(${routePage})`))
+        showEl(pageEl, true);
+        pageEl.dispatchEvent(new Event('routePageLoaded'));
+    }, false);
+    document.getElementById('loginPageBtn').addEventListener('click', () => changePage('signIn'));
+    const usernameContainer = document.getElementById('usernameContainer');
+    const anonBtn = document.getElementById('anonBtn');
+    const googleBtn = document.getElementById('googleBtn');
+    document.getElementById('loginForm').addEventListener('change', ({target}) => {
+        switch (target.value) {
+            case 'google':
+                hideEls([usernameContainer, anonBtn]);
+                showEl(googleBtn);
+                break;
+            case 'anon':
+                showEl(usernameContainer);
+                hideEls([googleBtn]);
+                showEl(anonBtn);
+                break;
+            default:
+            //
+        }
+    });
+    const nameRegex = '^(?=[^A-Za-z]*[A-Za-z]{2,})[ -~]*$';
+    const usernameInput = document.getElementById('usernameInput');
+    usernameInput.setAttribute('pattern', nameRegex);
+
+    document.getElementById('signIn').addEventListener('routePageLoaded', () => {
+        if (localStorage.getItem('display_name')) {
+            usernameInput.value = localStorage.getItem('display_name');
+            usernameInput.focus();
+        }
+    })
+
+    // ** Anon Auth Init
+    const re = new RegExp(nameRegex);
+    const anonFormHandler = (e) => {
+        e.preventDefault();
+        // validate
+        if (re.test(usernameInput.value)) {
+            // remove extra spaces
+            const displayName = usernameInput.value.replace(/\s+/g, ' ').trim();
+            localStorage.setItem('display_name', displayName);  // save for next use
+            localStorage.setItem('auth_choice', 'anonymous');
+            returnToRequestedPage();
+        }
+    };
+    document.getElementById('loginForm').addEventListener('submit', anonFormHandler);
+
+    window.dispatchEvent(new Event('hashchange')); // Manually trigger initial hash routing
+});
