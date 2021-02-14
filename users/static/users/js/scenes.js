f923eb436010a15c8383e1080fd7c3f66ea92627
Ivan Liang <r33t@cmu.edu>
2021-02-14 15:40:29 -0500

    Add missing select2 init

--- users/static/users/js/scenes.js
+++ users/static/users/js/scenes.js
@@ -204,4 +204,14 @@ document.addEventListener('DOMContentLoaded', function () {   // document.ready(
 
 
     window.dispatchEvent(new Event('hashchange')); // Manually trigger initial hash routing
+
+    $('select').each(function () {
+        $(this).select2({
+          theme: 'bootstrap4',
+          width: $(this).data('width') ? $(this).data('width') : $(this).hasClass('w-100') ? '100%' : 'style',
+          placeholder: $(this).data('placeholder'),
+          allowClear: Boolean($(this).data('allow-clear')),
+          closeOnSelect: !$(this).attr('multiple'),
+        });
+  });
 });
