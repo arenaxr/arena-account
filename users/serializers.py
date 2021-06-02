339aa456b3742cf4d6f3a0e9549a704916b66cbd
mwfarb <mwfarb@andrew.cmu.edu>
2021-06-02 12:52:30 -0400

    Serialize scene Editors by username

--- users/serializers.py
+++ users/serializers.py
@@ -1,8 +1,16 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
+
 from users.models import Scene
 
 
 class SceneSerializer(serializers.ModelSerializer):
+    editors = serializers.SlugRelatedField(
+        queryset=User.objects.all(),
+        many=True,
+        slug_field='username'
+    )
+
     class Meta:
         model = Scene
         fields = [
