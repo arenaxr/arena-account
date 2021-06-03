1d4e0ecdffd8eebcaf43efc05e40d6a9cf636e02
mwfarb <mwfarb@andrew.cmu.edu>
2021-06-03 10:08:22 -0400

    Add admin page Editors list autocomplete

--- users/admin.py
+++ users/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import Scene
 
-admin.site.register(Scene)
+
+class SceneAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['editors']
+
+
+admin.site.register(Scene, SceneAdmin)
