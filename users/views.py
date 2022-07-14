7ab1d0bd62fb717629fdacd365a6c3a33868d392
mwfarb <mwfarb@andrew.cmu.edu>
2022-07-14 11:50:40 -0400

    fix: redirect logged in user to /scenes

--- users/views.py
+++ users/views.py
@@ -50,25 +50,28 @@ def login_request(request):
     """
     Login page load, handles user/pass login if required.
     """
-    if request.method == "POST":
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get("username")
-            password = form.cleaned_data.get("password")
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                messages.info(
-                    request, f"You are now logged in as {username}.")
-                return redirect("login_callback")
+    if request.user.is_authenticated:
+        return redirect(f"https://{request.get_host()}/scenes")
+    else:
+        if request.method == "POST":
+            form = AuthenticationForm(request, data=request.POST)
+            if form.is_valid():
+                username = form.cleaned_data.get("username")
+                password = form.cleaned_data.get("password")
+                user = authenticate(username=username, password=password)
+                if user is not None:
+                    login(request, user)
+                    messages.info(
+                        request, f"You are now logged in as {username}.")
+                    return redirect("login_callback")
+                else:
+                    messages.error(request, "Invalid username or password.")
             else:
                 messages.error(request, "Invalid username or password.")
-        else:
-            messages.error(request, "Invalid username or password.")
-    form = AuthenticationForm()
-    return render(
-        request=request, template_name="users/login.html", context={"login_form": form}
-    )
+        form = AuthenticationForm()
+        return render(
+            request=request, template_name="users/login.html", context={"login_form": form}
+        )
 
 
 def logout_request(request):
