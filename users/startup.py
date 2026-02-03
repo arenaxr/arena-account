3151ffa044fb1927fe63a887cd05a0b1e66d5bed
mwfarb <mwfarb@andrew.cmu.edu>
2026-02-03 12:56:19 -0500

    fix(scenes): remove legacy redundant default scenes from db

--- users/startup.py
+++ users/startup.py
@@ -3,6 +3,14 @@ import os
 from allauth.socialaccount.models import SocialApp
 from django.conf import settings
 from django.contrib.sites.models import Site
+from users.models import (
+    SCENE_ANON_USERS_DEF,
+    SCENE_PUBLIC_READ_DEF,
+    SCENE_PUBLIC_WRITE_DEF,
+    SCENE_USERS_DEF,
+    SCENE_VIDEO_CONF_DEF,
+    Scene,
+)
 
 
 def setup_socialapps():
@@ -17,7 +25,9 @@ def setup_socialapps():
             # check that another site is not using our host, remove
             host_inst = Site.objects.filter(domain=host)
             if host_inst.exists():
-                host_inst.delete()
+                count, _ = host_inst.delete()
+                if count > 0:
+                    print(f"Deleted Site entries matching host: {count}")
             # update proper site_id with host
             id_inst.name = host
             id_inst.domain = host
@@ -27,4 +37,19 @@ def setup_socialapps():
         id_inst.save()
 
     # social apps are defined in settings.py, so remove legacy allauth entires that might conflict
-    SocialApp.objects.filter(provider='google').delete()
+    count, _ = SocialApp.objects.filter(provider='google').delete()
+    if count > 0:
+        print(f"Deleted legacy SocialApp entries: {count}")
+
+    # check for scenes where Scene.is_default is True and remove them
+    count, _ = Scene.objects.filter(
+        public_read=SCENE_PUBLIC_READ_DEF,
+        public_write=SCENE_PUBLIC_WRITE_DEF,
+        anonymous_users=SCENE_ANON_USERS_DEF,
+        video_conference=SCENE_VIDEO_CONF_DEF,
+        users=SCENE_USERS_DEF,
+        editors__isnull=True,
+        viewers__isnull=True,
+    ).delete()
+    if count > 0:
+        print(f"Deleted redundant default Scene entries: {count}")
